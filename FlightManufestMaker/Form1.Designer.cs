f7e7d9f008f4cf43b1982a236737b1b7a8e148ca
Nuwan Obeyesekera <nuwan.obeyesekera@scicom.com.my>
2024-07-04 14:45:44 +0530

    Add project files.

---
+++ FlightManufestMaker/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace FlightManufestMaker
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtShipFlight = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPaxCount = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.radShip = new System.Windows.Forms.RadioButton();
+            this.radFlight = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLocalNat = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 183);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1296, 604);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(885, 30);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(158, 74);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(169, 30);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(59, 25);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Name";
+            // 
+            // txtShipFlight
+            // 
+            this.txtShipFlight.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtShipFlight.Location = new System.Drawing.Point(169, 63);
+            this.txtShipFlight.Name = "txtShipFlight";
+            this.txtShipFlight.Size = new System.Drawing.Size(150, 31);
+            this.txtShipFlight.TabIndex = 4;
+            this.txtShipFlight.TextChanged += new System.EventHandler(this.txtShipFlight_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(378, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Pax Count";
+            // 
+            // txtPaxCount
+            // 
+            this.txtPaxCount.Location = new System.Drawing.Point(378, 63);
+            this.txtPaxCount.Name = "txtPaxCount";
+            this.txtPaxCount.Size = new System.Drawing.Size(150, 31);
+            this.txtPaxCount.TabIndex = 6;
+            this.txtPaxCount.TextChanged += new System.EventHandler(this.txtPaxCount_TextChanged);
+            this.txtPaxCount.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPaxCount_KeyPress);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(1076, 30);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(158, 74);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // radShip
+            // 
+            this.radShip.AutoSize = true;
+            this.radShip.Location = new System.Drawing.Point(22, 30);
+            this.radShip.Name = "radShip";
+            this.radShip.Size = new System.Drawing.Size(72, 29);
+            this.radShip.TabIndex = 8;
+            this.radShip.TabStop = true;
+            this.radShip.Text = "Ship";
+            this.radShip.UseVisualStyleBackColor = true;
+            this.radShip.CheckedChanged += new System.EventHandler(this.radShip_CheckedChanged);
+            // 
+            // radFlight
+            // 
+            this.radFlight.AutoSize = true;
+            this.radFlight.Location = new System.Drawing.Point(22, 65);
+            this.radFlight.Name = "radFlight";
+            this.radFlight.Size = new System.Drawing.Size(81, 29);
+            this.radFlight.TabIndex = 9;
+            this.radFlight.TabStop = true;
+            this.radFlight.Text = "Flight";
+            this.radFlight.UseVisualStyleBackColor = true;
+            this.radFlight.CheckedChanged += new System.EventHandler(this.radFlight_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(586, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Local Nationality";
+            // 
+            // txtLocalNat
+            // 
+            this.txtLocalNat.Location = new System.Drawing.Point(586, 63);
+            this.txtLocalNat.Name = "txtLocalNat";
+            this.txtLocalNat.Size = new System.Drawing.Size(150, 31);
+            this.txtLocalNat.TabIndex = 11;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1320, 799);
+            this.Controls.Add(this.txtLocalNat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radFlight);
+            this.Controls.Add(this.radShip);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtPaxCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtShipFlight);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Pax Manifest Maker v1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtShipFlight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPaxCount;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RadioButton radShip;
+        private System.Windows.Forms.RadioButton radFlight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLocalNat;
+    }
+}
+
---
+++ FlightManufestMaker/Form1.cs
@@ -0,0 +1,486 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExcelApp = Microsoft.Office.Interop.Excel;
+
+
+namespace FlightManufestMaker
+{
+    public partial class frmMain : Form
+    {
+        DataTable dgTable;
+
+        //Create COM Objects to write manifest.
+        //ExcelApp.Application manifestExcel = new ExcelApp.Application();
+        //ExcelApp.Workbook manifestWorkbook;
+        //ExcelApp.Worksheet manifestWorksheet;
+        //ExcelApp.Range manifestCellrange;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            
+        }
+
+        //Create COM Objects to read countries.
+        //ExcelApp.Application countryExcel = new ExcelApp.Application();
+        DataRow myNewRow;
+        DataTable myTable;
+        private DataTable dtCountry;
+
+        string localNat;
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            radShip.Checked = true;
+
+            btnGenerate.Enabled = false;
+            btnSave.Enabled = false;
+
+            //txtPaxCount.Text = "5";
+            //txtShipFlight.Text = "UL256";
+
+            localNat = ConfigurationManager.AppSettings["localNat"];
+            txtLocalNat.Text = localNat;
+            txtLocalNat.ReadOnly = true;
+
+            loadCountries();
+        }
+
+        private DataTable getShipManifestColums()
+        {
+            DataTable table = new System.Data.DataTable();
+            table.Columns.Add("ID", typeof(string));
+            table.Columns.Add("GIVENNAME", typeof(string));
+            table.Columns.Add("LASTNAME", typeof(string));
+            table.Columns.Add("NATIONALITY", typeof(string));
+            table.Columns.Add("GENDER", typeof(string));
+            table.Columns.Add("DOB", typeof(string));
+            table.Columns.Add("TYPE", typeof(string));
+            table.Columns.Add("DOCUMENTNO", typeof(string));
+            table.Columns.Add("DOCUMENTTYPE", typeof(string));
+            table.Columns.Add("EXPIRYDATE", typeof(string));
+            
+            return table;
+        }
+
+        private DataTable getFlightManifestColums()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("ID", typeof(string));
+            table.Columns.Add("GIVENNAME", typeof(string));
+            table.Columns.Add("LASTNAME", typeof(string));
+            table.Columns.Add("NATIONALITY", typeof(string));
+            table.Columns.Add("DOCUMENTNO", typeof(string));
+            table.Columns.Add("GENDER", typeof(string));
+            table.Columns.Add("DOB", typeof(string));
+            table.Columns.Add("TYPE", typeof(string));
+            table.Columns.Add("DOCUMENTTYPE", typeof(string));
+            table.Columns.Add("EXPIRYDATE", typeof(string));
+
+            return table;
+        }
+
+        private void addRows(int paxCount)
+        {
+            for (int i = 1; i <= paxCount; i++)
+            {
+
+
+                DataRow country = rndCountry();
+                string alpha2 = country[1].ToString();
+                string alpha3 = country[2].ToString();
+                string paxType = "FOREIGN";
+                string docType = "NP";
+
+                if (alpha3 == localNat)
+                {
+                    paxType = "LOCAL";
+                }
+
+                if (radShip.Checked)
+                {
+                        dgTable.Rows.Add(i, generateName(5), generateName(7), alpha3, rndGender(), rndDOB(), paxType, rndTravelDoc(alpha2), docType, rndExpiryDate());
+                }
+                else
+                {
+                        dgTable.Rows.Add(i, generateName(5), generateName(7), alpha3, rndTravelDoc(alpha2), rndGender(), rndDOB(), paxType, docType, rndExpiryDate());
+                }
+
+            }
+        }
+
+        public static string generateName(int len)
+        {
+            Random r = new Random();
+            string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "l", "n", "p", "q", "r", "s", "sh", "zh", "t", "v", "w", "x" };
+            string[] vowels = { "a", "e", "i", "o", "u", "ae", "y" };
+            string Name = "";
+            Name += consonants[r.Next(consonants.Length)].ToUpper();
+            Name += vowels[r.Next(vowels.Length)];
+            int b = 2; //b tells how many times a new letter has been added. It's 2 right now because the first two letters are already in the name.
+            while (b < len)
+            {
+                Name += consonants[r.Next(consonants.Length)];
+                b++;
+                Name += vowels[r.Next(vowels.Length)];
+                b++;
+            }
+
+            return Name;
+        }
+
+        private string rndGender()
+        {
+            string[] genders = { "M", "F" };
+
+            int rNo = randomNumber(0, 1000);
+            int index = rNo % 2;
+            return genders[index];
+        }
+
+        private string rndDOB()
+        {
+            DateTime start = new DateTime(1945, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(gen.Next(range)).ToString("dd/MM/yyyy");
+        }
+
+        private string rndExpiryDate()
+        {
+            DateTime start = DateTime.Today;
+            int range = 100;
+            return start.AddDays(gen.Next(range)).ToString("dd/MM/yyyy");
+        }
+
+        private Random gen = new Random();
+
+        public int randomNumber(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+
+        private readonly Random _random = new Random();
+
+
+        private string rndTravelDoc(string prefix)
+        {
+            var tdBuilder = new StringBuilder();
+            tdBuilder.Append(prefix);
+            tdBuilder.Append(randomNumber(10000, 99999));
+
+            return tdBuilder.ToString();
+        }
+
+        private void saveManifest(string fileName)
+        {
+            ExcelApp.Application manifestExcel = new ExcelApp.Application();
+            ExcelApp.Workbook manifestWorkbook = manifestExcel.Workbooks.Add(Type.Missing);
+
+            try
+            {
+                //manifestExcel = new ExcelApp.Application();
+                manifestExcel.Visible = false;
+                manifestExcel.DisplayAlerts = false;
+                //manifestWorkbook = manifestExcel.Workbooks.Add(Type.Missing);
+
+
+                ExcelApp.Worksheet manifestWorksheet = (ExcelApp.Worksheet)manifestWorkbook.ActiveSheet;
+                //manifestWorksheet = (ExcelApp.Worksheet)manifestWorkbook.ActiveSheet;
+                manifestWorksheet.Name = "Passenger Data";
+
+                
+                manifestWorksheet.Cells[1, 1] = txtShipFlight.Text;
+                manifestWorksheet.Cells.Font.Size = 11;
+            
+
+            for (int i = 1; i <= dgTable.Columns.Count; i++)
+            {
+                manifestWorksheet.Cells[2, i] = dgTable.Columns[i - 1].ColumnName;
+                manifestWorksheet.Cells.Font.Color = System.Drawing.Color.Black;
+            }
+
+
+
+
+
+            int worksheetRow = 2;
+
+            foreach (DataRow datarow in dgTable.Rows)
+            {
+                worksheetRow += 1;
+                                               
+                    for (int i = 1; i <= dgTable.Columns.Count; i++)
+                    {
+                    manifestWorksheet.Cells[worksheetRow, i] = "'" + datarow[i - 1].ToString();
+                    
+                    }
+
+            }
+
+
+            
+
+                manifestWorkbook.SaveAs(fileName);
+                manifestWorkbook.Close();
+                //manifestExcel.Quit();
+
+            }
+
+            
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                //manifestWorksheet = null;
+                //manifestCellrange = null;
+                //manifestWorkbook = null;
+
+                //if (manifestWorkbook != null) System.Runtime.InteropServices.Marshal.ReleaseComObject(manifestWorkbook);
+                //GC.Collect();
+                manifestExcel?.Quit();
+                Thread.Sleep(5000);
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            txtShipFlight.ReadOnly = true;
+            txtPaxCount.ReadOnly = true;
+            btnGenerate.Enabled = false;
+            int paxCount = Int32.Parse(txtPaxCount.Text);
+            addRows(paxCount);
+            btnSave.Enabled = true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            btnSave.Enabled = false;
+            dataGridView1.ReadOnly = true;
+
+            string fileName;
+
+            if (radShip.Checked)
+            {
+                fileName = "SHIP_";
+                radFlight.Visible = false;
+            }
+            else
+            {
+                fileName = "FLIGHT_";
+                radShip.Visible = false;
+
+            }
+
+            fileName += txtShipFlight.Text + "_";
+
+            fileName += DateTime.Now.ToString("yyyyMMddHHmmss");
+
+            saveManifest(fileName);
+            MessageBox.Show(fileName + " Saved !");
+
+            radShip.Visible = true;
+            radFlight.Visible = true;
+            dataGridView1.ReadOnly = false;
+        }
+
+        private void radShip_CheckedChanged(object sender, EventArgs e)
+        {
+            radCheckedChange();
+        }
+
+        private void radFlight_CheckedChanged(object sender, EventArgs e)
+        {
+            radCheckedChange();
+        }
+
+        private void radCheckedChange()
+        {
+            btnGenerate.Enabled = false;
+            btnSave.Enabled = false;
+
+            if (radShip.Checked)
+            {
+                dgTable = getShipManifestColums();
+                lblName.Text = "Ship Name";
+                
+            }
+            else
+            {
+                dgTable = getFlightManifestColums();
+                lblName.Text = "Flight Name";
+
+            }
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = dgTable;
+
+            txtShipFlight.ReadOnly = false;
+            txtPaxCount.ReadOnly = false;
+
+            txtChanged();
+        }
+
+        private void txtShipFlight_TextChanged(object sender, EventArgs e)
+        {
+            txtChanged();
+        }
+
+        private void txtPaxCount_TextChanged(object sender, EventArgs e)
+        {
+            txtChanged();
+        }
+
+        private int getNameLength()
+        {
+            return txtShipFlight.Text.Length;
+        }
+
+        private int getPaxCount()
+        {
+            if (txtPaxCount.Text.Length>0)
+            {
+                return Int32.Parse(txtPaxCount.Text);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private void txtChanged()
+        {
+            if ((getNameLength() > 2)&&(getPaxCount()>0))
+            {
+                btnGenerate.Enabled = true;
+            }
+            else
+            {
+                btnGenerate.Enabled = false;
+            }
+        }
+
+        private void txtPaxCount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        
+
+        private void loadCountries()
+        {
+            try
+            {
+                var GetDirectory = AppContext.BaseDirectory;
+
+                dtCountry = ReadExcel(GetDirectory + "\\Countries.xlsx"); //read excel file
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private DataRow rndCountry()
+        {
+
+            Random r = new Random();
+            int rInt = r.Next(1, dtCountry.Rows.Count);
+
+            DataRow countryRow = dtCountry.Rows[rInt];
+
+            return countryRow;
+        }
+
+        private DataTable ReadExcel(string fileName)
+        {
+            ExcelApp.Application countryExcel = new ExcelApp.Application();
+            ExcelApp.Workbook excelBook = countryExcel.Workbooks.Open(fileName);
+
+            try
+            {
+                //Open Excel file
+
+                ExcelApp._Worksheet excelSheet = excelBook.Sheets[1];
+                ExcelApp.Range excelRange = excelSheet.UsedRange;
+
+                int rows = excelRange.Rows.Count;
+                int cols = excelRange.Columns.Count;
+
+                //Set DataTable Name and Columns Name
+                myTable = new DataTable(excelSheet.Name);
+
+                //first row using for heading
+                for (int i = 1; i <= cols; i++)
+                {
+                    myTable.Columns.Add(excelRange.Cells[1, i].Value2.ToString(), typeof(string));
+                }
+
+                if (rows > 1)
+                {
+                    //first row using for heading, start second row for data
+                    for (int r = 2; r <= rows; r++)
+                    {
+                        myNewRow = myTable.NewRow();
+                        for (int c = 1; c <= cols; c++)
+                        {
+                            string cellVal = "";
+
+                            if (myTable.Columns[c - 1].ColumnName.Contains("Date"))
+                            {
+                                DateTime conv = DateTime.FromOADate(excelRange.Cells[r, c].Value2);
+                                cellVal = conv.ToShortDateString();
+                            }
+                            else
+                            {
+                                cellVal = Convert.ToString(excelRange.Cells[r, c].Value2);
+                            }
+                            myNewRow[c - 1] = cellVal;
+
+
+                        }
+                        myTable.Rows.Add(myNewRow);
+                    }
+                }
+                else
+                {
+                    DataRow newrow = myTable.NewRow();
+
+                    for (int i = 0; i < cols; i++)
+                    {
+                        newrow[i] = "";
+                    }
+
+                    myTable.Rows.Add(newrow);
+                }
+
+                excelBook.Close();
+                return myTable;
+            }
+            finally
+            {
+                //if (excelBook != null) System.Runtime.InteropServices.Marshal.ReleaseComObject(excelBook);
+                //GC.Collect();
+                countryExcel?.Quit();
+                Thread.Sleep(5000);
+            }
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            GC.Collect();
+
+        }
+    }
+}
